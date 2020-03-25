510dbf05065b37a4fe1ad60ead142aeedfc127ff
ForelaxX <helloworldmkv@gmail.com>
2020-03-25 23:38:13 +0800

    init

---
+++ tampermonkey-dida365-time2do.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         dida365-time2do
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  滴答清单的时间统计脚本
+// @author       Forelax
+// @match        https://dida365.com/
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
